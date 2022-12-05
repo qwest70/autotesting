a4f2a0374ed59836a540379bfc53814f6a4625ba
kosov.as <kosov.as@micran.ru>
2022-12-05 15:32:42 +0700

    тест автосоздания большого количества станций

--- master_m_tests/autocreate_snmp_stations.py
+++ master_m_tests/autocreate_snmp_stations.py
@@ -13,30 +13,30 @@ with open('version.txt') as file:
 #         send_keys('{ENTER}')
 #         time.sleep(1)
 
-k = 151
+k = 207
 for i in range(51): # тут указываем нужное количество станций, но не более 130, т.к. после, иконки станций займут всё пространство карты
-    with Window(u"Мастер М Версия: 0.0.0 beta (Administrator)||Window"):
+    with Window(u"Master M Version: 0.0.0 beta (Administrator)||Window"):
         with Region(u"||Custom->||Custom->||Pane->||Custom"):
             right_click(u"||Custom#[0,0]")
 
     with Window(u"||Menu"):
-        left_click(u"Добавить станцию||MenuItem")
-    with Window(u"Новая станция||Window"):
-        left_click(u"Имя||Edit")
-        left_click(u"IP адрес||Edit")
+        left_click(u"Add station||MenuItem")
+    with Window(u"New station||Window"):
+        left_click(u"Name||Edit")
+        left_click(u"IP address||Edit")
         send_keys('192.168.0.' + str(k))
         k = 1 + k
         with Region(u"||Custom->||Custom->Community||Group"):
-            left_click(u"Запись||Edit")
-        left_click(u"||Custom->||Custom->Community||Group->Запись||Edit")
+            left_click(u"Write||Edit")
+        left_click(u"||Custom->||Custom->Community||Group->Write||Edit")
         send_keys('{BACKSPACE}' * 6)
         send_keys('private')
-        left_click(u"Тип Вниз||ComboBox")
+        left_click(u"Type Вниз||ComboBox")
         time.sleep(1)
         send_keys('{DOWN}' * 18)
         time.sleep(1)
         send_keys('{ENTER}')
 
 
-        left_click(u"Создать Enter||Button")
+        left_click(u"Create Enter||Button")
 
